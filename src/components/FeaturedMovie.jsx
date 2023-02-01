5b0b0d51a162da9ed302dcee5ca14aa0e4e1290c
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-01 15:04:09 -0300

    Fixing deploy

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./FeaturedMovie.css";
 
 const FeaturedMovie = ({ item }) => {
+  console.log(item);
   let firstDate = new Date(item.first_air_date);
   let genres = [];
   for (let i in item.genres) {
